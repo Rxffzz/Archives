87b5fe94bb750b1c5c399ea8e8a7a0d52ebdb411
Rxffzz <srafael0406@gmail.com>
2024-08-21 22:20:07 -0300

    Using functions of class File

---
+++ Files.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Files {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a file path: ");
+        String pathFile = sc.nextLine();
+
+        File path = new File(pathFile);
+
+        System.out.println("getName: " + path.getName());
+        System.out.println("getParent: " + path.getParent());
+        System.out.println("getPath:  " + path.getPath());
+
+        sc.close();
+    }
+}
